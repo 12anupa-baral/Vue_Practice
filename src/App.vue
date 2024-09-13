87de83c8ba3962b0d1c260dd841f8771f6960f81
Anupa Baral <86550755+12anupa-baral@users.noreply.github.com>
2024-09-13 19:50:21 +0545

    fundamental learnt

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <!-- composition api -->
 
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
     const name = ref('Anupa Baral');
     const status =ref('active');
     const tasks =ref(['task 1','task 2','task 3']);
+    const newTask = ref('neww');
 
     const toggleStatus = () =>{
       if(status.value === 'active'){
@@ -19,22 +20,59 @@ import { ref } from 'vue';
       }
     };
 
+    const addTask =()=>{
+      if(newTask.value.trim()!==''){
+        tasks.value.push(newTask.value);
+        newTask.value='';
+      }
+    }
+
+    const deleteTask=(index)=>{
+      tasks.value.splice(index ,1);
+
+    };
+
+    onMounted(async ()=>{
+      try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const data =await response.json();
+        tasks.value =data.map((task)=>task.title);
+      }
+      catch(error){
+        console.log('Error Fetching tasks');
+      }
+    });
+
    
 
 </script>
 
 <template>
   <h1>{{name}}</h1>
+  <br/>
 
   <!-- conditional rendering -->
   <p v-if="status === 'active'"> User is active</p>
   <p v-else-if="status === 'pending'"> User is pending</p>
   <p v-else> User is inactive</p>
+<br/>
+<form @submit.prevent="addTask">
+  <label for="newTask">Add Task</label>
+  <input type="text" id="newTask" v-model="newTask">
+  <br/>
+  <button type="submit">Submit</button>
 
+</form>
+
+<br/>
   <!-- for loop   -->
   <h3>Tasks:</h3> 
+  <br/>
   <ul>
-    <li v-for="task in tasks" key="task">{{ task }}</li>
+    <li v-for="task in tasks" key="task">
+     <span>{{ task }}</span> 
+    <button @click="deleteTask(index)">x</button>
+    </li>
   </ul>
 
 
@@ -42,6 +80,7 @@ import { ref } from 'vue';
   <!-- <button v-on:click="toogleStatus">Change Status</button> -->
 
   <!-- shortform -->
+<br/>
   <button @click="toggleStatus">Change Status</button>
 
   
