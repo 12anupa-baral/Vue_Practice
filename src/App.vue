a9494ead954d1db7d236ec5fd03c46fddeefab29
Anupa Baral <86550755+12anupa-baral@users.noreply.github.com>
2024-09-14 12:20:56 +0545

    added props

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
 import Navbar from './components/Navbar.vue';
+import Hero from './components/Hero.vue';
+import HomeCards from './components/HomeCards.vue';
+import Joblistings from './components/Joblistings.vue';
 </script>
 
 <template>
  <Navbar />
+ <Hero />
+ <HomeCards/>
+ <Joblistings/>
 </template>
